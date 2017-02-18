36a81e87cc49b2a482f3dbf9fe3f36bd24759c77
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-02-18 20:53:04 +0200

    switch to use custom tokio-proto version

--- src/client.rs
+++ src/client.rs
@@ -6,14 +6,16 @@ use futures::Future;
 use tokio_core::reactor::Handle;
 use tokio_core::io::{Framed, Io};
 use tokio_core::net::TcpStream;
-
+use tokio_core::io::{Codec, EasyBuf};
 use tokio_proto::TcpClient;
-use tokio_proto::pipeline::{ClientProto, ClientService};
-
+use tokio_proto::multiplex::{ClientProto, ClientService};
+use tokio_proto::streaming::multiplex::RequestIdSource;
 use tokio_service::Service;
 
 use package::Package;
 use codec::PackageCodec;
+
+use uuid::Uuid;
 
 pub struct EventStoreClient {
     inner: ClientService<TcpStream, PackageProto>,
@@ -66,16 +68,46 @@ impl<T> Stream for Heartbeats<T>
     }
 }
 */
+
+struct Separator;
+
+impl Codec for Separator {
+    type In = (Uuid, Package);
+    type Out = (Uuid, Package);
+
+    fn decode(&mut self, buf: &mut EasyBuf) -> io::Result<Option<Self::In>> {
+        PackageCodec.decode(buf).map(|x| x.map(|x| (x.correlation_id, x)))
+    }
+
+    fn encode(&mut self, msg: (Uuid, Package), buf: &mut Vec<u8>) -> io::Result<()> {
+        let (id, pkg) = msg;
+        assert_eq!(id, pkg.correlation_id);
+        PackageCodec.encode(pkg, buf)
+    }
+}
+
+impl RequestIdSource<Uuid, Package> for Separator {
+    fn next(&mut self, pkg: &Package) -> Uuid {
+        pkg.correlation_id
+    }
+}
+
 struct PackageProto;
 
 impl<T: Io + 'static> ClientProto<T> for PackageProto {
     type Request = Package;
     type Response = Package;
+    type RequestId = Uuid;
 
-    type Transport = Framed<T, PackageCodec>;
+    type Transport = Framed<T, Separator>;
     type BindTransport = Result<Self::Transport, io::Error>;
+    type RequestIds = Separator;
 
     fn bind_transport(&self, io: T) -> Self::BindTransport {
-        Ok(io.framed(PackageCodec))
+        Ok(io.framed(Separator))
+    }
+
+    fn requestid_source(&self) -> Self::RequestIds {
+        Separator
     }
 }
