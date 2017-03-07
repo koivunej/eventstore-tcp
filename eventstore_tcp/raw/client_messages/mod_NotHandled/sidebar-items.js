5b2719028b3967548cc1b35cd8e1cf32b34b6785
Joonas Koivunen <joonas.koivunen@gmail.com>
2017-03-07 23:18:54 +0200

    refresh docs now that delete has been implemented: d267a0a97ac868fdf3e31ce285de1113c1c548c5

---
+++ eventstore_tcp/raw/client_messages/mod_NotHandled/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["NotHandledReason",""]],"struct":[["MasterInfo",""]]});
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["eventstore_tcp"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='trait std::error::Error'>Error</a> for <a class='enum' href='eventstore_tcp/enum.WriteEventsFailure.html' title='enum eventstore_tcp::WriteEventsFailure'>WriteEventsFailure</a>",];
+implementors["eventstore_tcp"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='trait std::error::Error'>Error</a> for <a class='enum' href='eventstore_tcp/adapted/enum.WriteEventsFailure.html' title='enum eventstore_tcp::adapted::WriteEventsFailure'>WriteEventsFailure</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
